ef764aff9eee381a81babc0d0494943dd62483ff
Mukul Rai <shivam88158@gmail.com>
2025-01-26 21:05:58 +0530

    admin

--- components/ProductForm.tsx
+++ components/ProductForm.tsx
@@ -6,7 +6,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Separator } from '@/components/ui/separator';
-import { ImagePlus, X } from 'lucide-react';
+
 import { Textarea } from '@/components/ui/textarea';
 import {
   Select,
