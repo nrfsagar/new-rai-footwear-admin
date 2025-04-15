a6a2b2d83c347b322d5583e990a751922e7b5797
Mukul Rai <shivamrai88158@gmail.com>
2025-04-15 14:03:59 +0530

    xvalue

--- app/api/notifications/[id]/route.ts
+++ app/api/notifications/[id]/route.ts
@@ -1,33 +1,33 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { connectToDatabase } from "@/lib/mongoose";
-import AppNotification from "@/lib/models/notification.model";
+// import { NextRequest, NextResponse } from 'next/server';
+// import { connectToDatabase } from "@/lib/mongoose";
+// import AppNotification from "@/lib/models/notification.model";
 
-export async function PATCH(
-  req: NextRequest,
-  { params }: { params: { id: string } }
-): Promise<NextResponse> {
-  try {
-    await connectToDatabase();
-    const body = await req.json();
-    const updatedDevice = await AppNotification.findByIdAndUpdate(
-      params.id,
-      { $set: body },
-      { new: true }
-    );
+// export async function PATCH(
+//   req: NextRequest,
+//   { params }: { params: { id: string } }
+// ): Promise<NextResponse> {
+//   try {
+//     await connectToDatabase();
+//     const body = await req.json();
+//     const updatedDevice = await AppNotification.findByIdAndUpdate(
+//       params.id,
+//       { $set: body },
+//       { new: true }
+//     );
 
-    if (!updatedDevice) {
-      return NextResponse.json(
-        { error: 'Device not found' },
-        { status: 404 }
-      );
-    }
+//     if (!updatedDevice) {
+//       return NextResponse.json(
+//         { error: 'Device not found' },
+//         { status: 404 }
+//       );
+//     }
 
-    return NextResponse.json({ success: true, device: updatedDevice });
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json(
-      { error: 'Update failed' },
-      { status: 500 }
-    );
-  }
-}
+//     return NextResponse.json({ success: true, device: updatedDevice });
+//   } catch (error) {
+//     console.log(error);
+//     return NextResponse.json(
+//       { error: 'Update failed' },
+//       { status: 500 }
+//     );
+//   }
+// }
