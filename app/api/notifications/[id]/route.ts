683747b6501cf6106aee817371c8a93d189b6062
Mukul Rai <shivamrai88158@gmail.com>
2025-04-15 14:07:06 +0530

    xvalue

--- app/api/notifications/[id]/route.ts
+++ app/api/notifications/[id]/route.ts
@@ -1,33 +1,40 @@
-// import { NextRequest, NextResponse } from 'next/server';
-// import { connectToDatabase } from "@/lib/mongoose";
-// import AppNotification from "@/lib/models/notification.model";
+import { NextRequest, NextResponse } from 'next/server';
+import { connectToDatabase } from "@/lib/mongoose";
+import AppNotification from "@/lib/models/notification.model";
 
-// export async function PATCH(
-//   req: NextRequest,
-//   { params }: { params: { id: string } }
-// ): Promise<NextResponse> {
-//   try {
-//     await connectToDatabase();
-//     const body = await req.json();
-//     const updatedDevice = await AppNotification.findByIdAndUpdate(
-//       params.id,
-//       { $set: body },
-//       { new: true }
-//     );
+interface RouteParams {
+  params: {
+    id: string;
+  };
+}
 
-//     if (!updatedDevice) {
-//       return NextResponse.json(
-//         { error: 'Device not found' },
-//         { status: 404 }
-//       );
-//     }
+export async function PATCH(
+  req: NextRequest,
+  context: RouteParams
+): Promise<NextResponse> {
+  try {
+    await connectToDatabase();
+    const { id } = context.params;
+    const body = await req.json();
+    const updatedDevice = await AppNotification.findByIdAndUpdate(
+      id,
+      { $set: body },
+      { new: true }
+    );
 
-//     return NextResponse.json({ success: true, device: updatedDevice });
-//   } catch (error) {
-//     console.log(error);
-//     return NextResponse.json(
-//       { error: 'Update failed' },
-//       { status: 500 }
-//     );
-//   }
-// }
+    if (!updatedDevice) {
+      return NextResponse.json(
+        { error: 'Device not found' },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json({ success: true, device: updatedDevice });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: 'Update failed' },
+      { status: 500 }
+    );
+  }
+}
